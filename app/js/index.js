d779f40a0575547a813628d0eefa98ad02bb41c1
SaadDamine <m.sdaminesaad@gmail.com>
2021-02-06 13:15:37 +0100

    Arrays in js

--- app/js/index.js
+++ app/js/index.js
@@ -4,18 +4,28 @@ require('styles/main.scss');
 import $ from 'jquery';
 import {log, logTitle} from 'logger';
 /* your imports */
-logTitle('Title')
+logTitle('Arrays')
 /* coding examples */
 
-/* Variables */
+/* Arrays */
 
-var name = "Damine mohamed";
-var age = 21.5;
-var hasDRiverLicence = true;
-var empty = undefined
+var names = ["Damine", "mohamed", "Saad", 21,true]
 
-log(name + " type= " + typeof name);
-log(age + " type= " + typeof age);
-log(hasDRiverLicence + " type= " + typeof hasDRiverLicence);
-log(empty + " type= " + typeof empty);
+log(names + " type= " + typeof names);
 
+
+log("name in p 2 is "+names[2]);
+log("has size "+ names.length);
+
+log("normal loop ")
+
+//loop for
+for(var n of names){
+    log(n + " type= " + typeof n);
+}
+
+log("forEach loop ")
+
+names.forEach(function(n,index) {
+    log(n + " type= " + typeof n +" with index "+ index);
+})
